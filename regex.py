479053862abb658b1e1ac15a6799671977b7993c
jayapalredd <123534744+jayapalredd@users.noreply.github.com>
2023-01-25 14:32:01 +0530

    Add files via upload

---
+++ regex.py
@@ -0,0 +1,22 @@
+# import threading
+# from threading import *
+# from time import sleep
+
+
+# class thread(Thread):
+#     def run(self):
+#         for i in range(1,10):
+#             print(i,"this is 1st number")
+#             sleep(1)
+            
+# class thread2(Thread):
+#     def run(self):
+#         for i in range(1,10):
+#             print(i,"this is second number")
+#             sleep(1)
+# t1=thread()
+# t2=thread2()
+# t1.start()
+# # sleep(1)
+# t2.start()
+# # sleep(1)
